d3e412d54571efcbc592b13bbfe363bdaf5e6967
Patricklalaina <patrickrandri0@gmail.com>
2025-08-08 19:45:44 +0300

    Ajout des fonctionnalites de fenetre multiple, priorisation du fenetre drag

---
+++ script/class.js
@@ -0,0 +1,63 @@
+let	createdWin = {}
+
+class win {
+	_parent
+	_instance
+	_title
+	_name
+	_offset = {x:0, y:0}
+	_isDragging = false
+	constructor(name, parent){
+		this._parent = parent;
+		this._name = name;
+		let content = `\
+			<div class="head">\
+				<div class="title title-${name}"><p>${name}</p></div>\
+				<div class="close close-${name}" title="Fermer cette fenetre ?">\
+					<i class="fa-solid fa-xmark"></i>\
+				</div>\
+			</div>\
+			<div class="wbody"></div>`;
+		
+		this._instance = document.createElement("div");
+		this._instance.classList.add('window');
+		this._instance.classList.add(name);
+		this._instance.innerHTML = content;
+		parent.appendChild(this._instance);
+
+		this._instance.style.zIndex = '30';
+		this._title = document.querySelector(`.title-${name}`);
+
+		let close = document.querySelector(`.close-${name}`);
+		close.addEventListener('click', ()=>{
+			delete createdWin[this._name];
+			parent.removeChild(this._instance);
+		});
+
+		this._title.addEventListener("mousedown", (e)=>{
+			this._isDragging = true;
+			this._offset.x = e.clientX - this._instance.offsetLeft;
+			this._offset.y = e.clientY - this._instance.offsetTop;
+			document.body.style.userSelect = "none"; // évite de sélectionner du texte en même temps
+			for (let n of Object.keys(createdWin)){
+				createdWin[n].thisInstance().style.zIndex = '10';
+			}
+			this._instance.style.zIndex = '20';
+		});
+
+		document.addEventListener("mousemove", (e)=>{
+			if (!this._isDragging) return;
+			this._instance.style.left = (e.clientX - this._offset.x) + "px";
+			this._instance.style.top = (e.clientY - this._offset.y) + "px";
+		});
+
+		document.addEventListener("mouseup", ()=>{
+			this._isDragging = false;
+			document.body.style.userSelect = ""; // réactive la sélection
+		});
+	}
+
+	thisInstance(){
+		return this._instance;
+	}
+}
--- script/script.js
+++ script/script.js
@@ -1,30 +1,85 @@
-const win = document.querySelector('.window')
-const titleBar = document.querySelector('.title')
-const closeBtn = document.querySelector('.close')
-const container = document.querySelector('.container')
-let offsetX = 0;
-let offsetY = 0;
-let isDragging = false;
+// let	createdWin = {}
 
-titleBar.addEventListener("mousedown", function (e) {
-	isDragging = true;
-	offsetX = e.clientX - win.offsetLeft;
-	offsetY = e.clientY - win.offsetTop;
-	document.body.style.userSelect = "none"; // évite de sélectionner du texte en même temps
-});
+// class win {
+// 	_parent
+// 	_instance
+// 	_title
+// 	_name
+// 	_offset = {x:0, y:0}
+// 	_isDragging = false
+// 	constructor(name, parent){
+// 		this._parent = parent;
+// 		this._name = name;
+// 		let content = `\
+// 			<div class="head">\
+// 				<div class="title title-${name}"><p>${name}</p></div>\
+// 				<div class="close close-${name}" title="Fermer cette fenetre ?">\
+// 					<i class="fa-solid fa-xmark"></i>\
+// 				</div>\
+// 			</div>\
+// 			<div class="wbody"></div>`;
+		
+// 		this._instance = document.createElement("div");
+// 		this._instance.classList.add('window');
+// 		this._instance.classList.add(name);
+// 		this._instance.innerHTML = content;
+// 		parent.appendChild(this._instance);
 
-document.addEventListener("mousemove", function (e) {
-	if (!isDragging) return;
+// 		this._instance.style.zIndex = '30';
+// 		this._title = document.querySelector(`.title-${name}`);
 
-	win.style.left = (e.clientX - offsetX) + "px";
-	win.style.top = (e.clientY - offsetY) + "px";
-});
+// 		let close = document.querySelector(`.close-${name}`);
+// 		close.addEventListener('click', ()=>{
+// 			delete createdWin[this._name];
+// 			parent.removeChild(this._instance);
+// 		});
 
-document.addEventListener("mouseup", function () {
-	isDragging = false;
-	document.body.style.userSelect = ""; // réactive la sélection
-});
+// 		this._title.addEventListener("mousedown", (e)=>{
+// 			this._isDragging = true;
+// 			this._offset.x = e.clientX - this._instance.offsetLeft;
+// 			this._offset.y = e.clientY - this._instance.offsetTop;
+// 			document.body.style.userSelect = "none"; // évite de sélectionner du texte en même temps
+// 			for (let n of Object.keys(createdWin)){
+// 				createdWin[n].thisInstance().style.zIndex = '10';
+// 			}
+// 			this._instance.style.zIndex = '20';
+// 		});
 
-closeBtn.addEventListener("click", ()=>{
-	container.removeChild(win)
-})
+// 		document.addEventListener("mousemove", (e)=>{
+// 			if (!this._isDragging) return;
+// 			this._instance.style.left = (e.clientX - this._offset.x) + "px";
+// 			this._instance.style.top = (e.clientY - this._offset.y) + "px";
+// 		});
+
+// 		document.addEventListener("mouseup", ()=>{
+// 			this._isDragging = false;
+// 			document.body.style.userSelect = ""; // réactive la sélection
+// 		});
+// 	}
+
+// 	thisInstance(){
+// 		return this._instance;
+// 	}
+// }
+
+
+
+const container = document.querySelector('.container');
+const home = document.querySelector('.home');
+const about = document.querySelector('.about');
+const project = document.querySelector('.project');
+const contact = document.querySelector('.contact');
+
+
+let addBtnEvent = (element, name, parent)=>{
+	element.addEventListener('click', ()=>{
+		if (!Object.keys(createdWin).includes(name)){
+			createdWin[name]= new win(name, parent);
+		}
+	})
+}
+
+addBtnEvent(home, "Presentation", container);
+addBtnEvent(about, "About", container);
+addBtnEvent(project, "Projects", container);
+addBtnEvent(contact, "Contact", container);
