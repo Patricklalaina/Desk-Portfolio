e67ee73b85aa46f8af5951f0d8f0a5ad04cd970e
Patricklalaina <patrickrandri0@gmail.com>
2025-09-06 01:57:03 +0300

    Add project
    init project design

--- script/content.js
+++ script/content.js
@@ -29,51 +29,60 @@ const hContent = `<div class="presentation-top">
 					</ul>
 				</div>`;
 const aContent = '';
-const pContent = '';
-const cContent = '<div class="contact-top">\
-					<h2>Contact me!</h2>\
-					<p>Looking for an ambitious and competent employee? Contact me..</p>\
-				</div>\
-				<div class="contact-bottom">\
-					<div class="left-contact">\
-						<div class="contact-item">\
-							<div class="contact-icon">\
-								<i class="fas fa-envelope"></i>\
-							</div>\
-							<div class="contact-details">\
-								<h4>Email</h4>\
-								<p>patrickrandri0@gmail.com</p>\
-							</div>\
-						</div>\
-						<div class="contact-item">\
-							<div class="contact-icon">\
-								<i class="fa-brands fa-linkedin"></i>\
-							</div>\
-							<div class="contact-details">\
-								<h4>Linkedin</h4>\
-								<a href="https://www.linkedin.com/in/patrick-randriamanambola">Patrick Randriamanambola</a>\
-							</div>\
-						</div>\
-						<div class="contact-item">\
-							<div class="contact-icon">\
-								<i class="fas fa-phone"></i>\
-							</div>\
-							<div class="contact-details">\
-								<h4>Telephone</h4>\
-								<p>+261 34 89 002 21</p>\
-							</div>\
-						</div>\
-					</div>\
-					<div style="width: 15%;margin: 10px;text-align: center;"><h3>- OR - </h3></div>\
-					<div class="right-contact">\
-						<form>\
-							<input type="email" name="email" id="email" placeholder="your@mail.com">\
-							<input type="text" name="objet" id="objet" placeholder="Object: ...">\
-							<textarea name="message" id="message" placeholder="your message ..."></textarea>\
-							<input type="submit" value="Submit">\
-						</form>\
-					</div>\
-				</div>';
+const pContent = `<div class="left">
+				<ul>
+					<li><i class="fas fa-angle-right"></i><i class="fas fa-folder"></i><p>Game Projects</p></li>
+					<li><i class="fas fa-angle-right"></i><i class="fas fa-folder"></i><p>Web Projects</p></li>
+					<li><i class="fas fa-angle-right"></i><i class="fas fa-folder"></i><p>Software Projects</p></li>
+					<li><i class="fas fa-angle-right"></i><i class="fas fa-folder"></i><p>Data Projects</p></li>
+				</ul>
+			</div>
+			<div class="right">
+			</div>`;
+const cContent = `<div class="contact-top">
+					<h2>Contact me!</h2>
+					<p>Looking for an ambitious and competent employee? Contact me..</p>
+				</div>
+				<div class="contact-bottom">
+					<div class="left-contact">
+						<div class="contact-item">
+							<div class="contact-icon">
+								<i class="fas fa-envelope"></i>
+							</div>
+							<div class="contact-details">
+								<h4>Email</h4>
+								<p>patrickrandri0@gmail.com</p>
+							</div>
+						</div>
+						<div class="contact-item">
+							<div class="contact-icon">
+								<i class="fa-brands fa-linkedin"></i>
+							</div>
+							<div class="contact-details">
+								<h4>Linkedin</h4>
+								<a href="https://www.linkedin.com/in/patrick-randriamanambola">Patrick Randriamanambola</a>
+							</div>
+						</div>
+						<div class="contact-item">
+							<div class="contact-icon">
+								<i class="fas fa-phone"></i>
+							</div>
+							<div class="contact-details">
+								<h4>Telephone</h4>
+								<p>+261 34 89 002 21</p>
+							</div>
+						</div>
+					</div>
+					<div style="width: 15%;margin: 10px;text-align: center;"><h3>- OR - </h3></div>
+					<div class="right-contact">
+						<form>
+							<input type="email" name="email" id="email" placeholder="your@mail.com">
+							<input type="text" name="objet" id="objet" placeholder="Object: ...">
+							<textarea name="message" id="message" placeholder="your message ..."></textarea>
+							<input type="submit" value="Submit">
+						</form>
+					</div>
+				</div>`;
 
 
 function checkContent(name){
